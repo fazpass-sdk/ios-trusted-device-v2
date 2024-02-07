9e6ff9d5a2576a12efce5063dfacd0f12c7537db
Widi <sulthanrayhan2009@gmail.com>
2024-02-07 16:04:27 +0700

    added user presence to access control flags

--- Sources/Fazpass/SecureUtil.swift
+++ Sources/Fazpass/SecureUtil.swift
@@ -9,7 +9,7 @@ internal class SecureUtil {
     private static let accessControl = SecAccessControlCreateWithFlags(
         kCFAllocatorDefault,
         kSecAttrAccessibleWhenPasscodeSetThisDeviceOnly,
-        .biometryCurrentSet,
+        [.biometryCurrentSet, .userPresence],
         nil)
     
     private let generateKeyAttributes: [String: Any] = [
--- ios-trusted-device-v2.podspec
+++ ios-trusted-device-v2.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |spec|
 spec.name       = "ios-trusted-device-v2"
-spec.version    = "1.1.7"
+spec.version    = "1.1.8"
 spec.summary    = "IOS Trusted Device"
 spec.description    = <<-DESC
 IOS counterpart for fazpass trusted device.
